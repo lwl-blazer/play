fe1bad8d6fc094b7917e75757fa945d34a679168
0268000108 <0268000108@zwxict.com>
2021-01-06 11:06:36 +0800

    init project

---
+++ Player/Player/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Player
+//
+//  Created by luowailin on 2021/1/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Player/Player/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Player
+//
+//  Created by luowailin on 2021/1/4.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
